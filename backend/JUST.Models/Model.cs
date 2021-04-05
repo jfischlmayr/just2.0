47d6b96ab658e25ee01c9aa9d1ca854a838eb65d
jfischlmayr <jan.fischlmayr@gmx.net>
2021-04-05 20:58:42 +0200

    added DTO's

--- backend/JUST.Models/Model.cs
+++ backend/JUST.Models/Model.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace backend.Models
+namespace JUST.Models
 {
-    public class Todo
+    public class TodoDto
     {
         [Key]
         public int ID { get; set; }
--- backend/backend/Controllers/TodosController.cs
+++ backend/backend/Controllers/TodosController.cs
@@ -1,4 +1,6 @@
-﻿using backend.Models;
+﻿using AutoMapper;
+using backend.Models;
+using JUST.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,33 +22,83 @@ namespace backend.Controllers
             _context = context;
         }
 
+        
+
+        // GET api/Todos
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Todo>))]
-        public async Task<IActionResult> GetAll()
+        public async Task<IQueryable<TodoDto>> GetTodosAsync()
         {
-            var list = await _context.Todos.ToListAsync();
-
-            return Ok(list.OrderBy(t => t.Name));
+            var todos = from t in _context.Todos
+                        select new TodoDto()
+                        {
+                            ID = t.ID,
+                            Name = t.Name,
+                            Done = t.Done
+                        };
+            return todos;
         }
 
-        [HttpGet("{id}", Name = nameof(GetById))]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Todo))]
+        // GET api/Todos/5
+        [HttpGet("{id}", Name = nameof(GetTodo))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TodoDto))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<IActionResult> GetTodo(int id)
         {
-            var existingTask = await _context.Todos.FirstOrDefaultAsync(t => t.ID == id);
-            if (existingTask == null) return NotFound();
-            return Ok(existingTask);
+            var configuration = new MapperConfiguration(cfg => {
+                cfg.CreateMap<Todo, TodoDto>();
+            });
+
+            IMapper iMapper = configuration.CreateMapper();
+
+            var todo = iMapper.Map<Todo, TodoDto>(await _context.Todos.FirstOrDefaultAsync(t => t.ID == id));
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todo);
         }
+
+        //[HttpGet]
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Todo>))]
+        //public async Task<IActionResult> GetAll()
+        //{
+        //    var list = await _context.Todos.ToListAsync();
+
+        //    return Ok(list.OrderBy(t => t.Name));
+        //}
+
+        //[HttpGet("{id}", Name = nameof(GetById))]
+        //[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Todo))]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public async Task<IActionResult> GetById(int id)
+        //{
+        //    var existingTask = await _context.Todos.FirstOrDefaultAsync(t => t.ID == id);
+        //    if (existingTask == null) return NotFound();
+        //    return Ok(existingTask);
+        //}
 
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Todo))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Add([FromBody] Todo newTask)
+        public async Task<IActionResult> Add([FromBody] Todo newTodo)
         {
-            await _context.Todos.AddAsync(newTask);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Todos.Add(newTodo);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetById), new { id = newTask.ID }, newTask);
+
+            var dto = new TodoDto()
+            {
+                ID = newTodo.ID,
+                Name = newTodo.Name,
+                Done = newTodo.Done
+            };
+
+            return CreatedAtAction(nameof(GetTodo), new { id = newTodo.ID }, dto);
         }
 
         [HttpPut("{id}")]
