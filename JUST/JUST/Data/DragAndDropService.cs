f2487160c2b849be9e5b3abf6c7b5fe900077c96
Samuel Kowatschek <samkow@gmx.at>
2021-05-14 17:06:12 +0200

    added dragAndDropService

---
+++ JUST/JUST/Data/DragAndDropService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JUST.Data
+{
+    public class DragAndDropService
+    {
+        public object Data { get; set; }
+        public string Zone { get; set; }
+
+        public void StartDrag(object data, string zone)
+        {
+            this.Data = data;
+            this.Zone = zone;
+        }
+
+        public bool Accepts(string zone)
+        {
+            return this.Zone == zone;
+        }
+    }
+}
