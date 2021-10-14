53233613bd18c169b558e8cfa3771e74e6fac454
samuel-kowatschek <samko@DESKTOP-DKP11P4>
2021-10-14 16:56:10 +0200

    added routing module

--- Frontend/just-reworked/src/app/app-routing.module.ts
+++ Frontend/just-reworked/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { GanttPageComponent } from './gantt-page/gantt-page.component';
+import { TaskPageComponent } from './task-page/task-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'gantt', component: GanttPageComponent},
+  {path: 'tasks', component: TaskPageComponent},
+  {path: 'projects', component: ProjectPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
