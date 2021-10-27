099d23346c35e4e00e940baccb74b0b7bf485769
samuel-kowatschek <samko@DESKTOP-DKP11P4>
2021-10-27 17:52:30 +0200

    added oldGantt Component to router

--- Frontend/just-reworked/src/app/app-routing.module.ts
+++ Frontend/just-reworked/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { GanttPageComponent } from './gantt-page/gantt-page.component';
 import { TaskPageComponent } from './task-page/task-page.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
+import { OldGanttComponent } from './old-gantt/old-gantt.component';
 
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'gantt', component: GanttPageComponent},
   {path: 'tasks', component: TaskPageComponent},
-  {path: 'projects', component: ProjectPageComponent}
+  {path: 'projects', component: ProjectPageComponent},
+  {path: 'oldGantt', component: OldGanttComponent}
 ];
 
 @NgModule({
