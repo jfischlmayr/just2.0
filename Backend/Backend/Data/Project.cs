4cf0bc8d2438e2ca686d8bde6c84067494c5d1d1
jfischlmayr <carreradigital0810>
2021-11-16 12:07:44 +0100

    added project model

---
+++ Backend/Backend/Data/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backend.Data
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
