2964cc3cf56545ae3bd96e4925e6db67612aee48
jfischlmayr <jan.fischlmayr@gmx.net>
2021-11-22 22:49:32 +0100

    frontend functionality

--- Frontend/just-reworked/src/app/app.module.ts
+++ Frontend/just-reworked/src/app/app.module.ts
@@ -8,21 +8,22 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { TaskPageComponent } from './task-page/task-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatCard, MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { OldGanttComponent } from './old-gantt/old-gantt.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,9 @@ import { FormsModule } from '@angular/forms';
     MatDividerModule,
     MatListModule,
     MatProgressBarModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent, HomePageComponent]
--- Frontend/just-reworked/src/app/project-page/project-page.component.ts
+++ Frontend/just-reworked/src/app/project-page/project-page.component.ts
@@ -1,10 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
-
-
-
-
+interface Project {
+  title: string
+  startDate: Date
+  endDate: Date
+}
 
 @Component({
   selector: 'app-project-page',
@@ -12,21 +16,35 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./project-page.component.scss']
 })
 export class ProjectPageComponent implements OnInit {
-
-
-
   showDelay = new FormControl(500);
 
-  public projectName : string = 'hurensohn'
+  title : string = ''
+  startDate: Date = new Date()
+  endDate: Date = new Date()
 
-  constructor() { }
+  projects!: Observable<Project[]>
+
+  constructor(private httpClient : HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.projects = this.httpClient.get<Project[]>('https://localhost:5001/api/project')
   }
-
 
   onSubmit(){
+    const project: Project = {
+      title: this.title,
+      startDate: this.startDate,
+      endDate: this.endDate
+    }
+
+    this.httpClient.post('https://localhost:5001/api/project', project).subscribe(() => this.router.navigate(['projects']))
+  }
+
+  editProject(p: Project){
 
   }
 
+  deleteProject(p: Project){
+    this.httpClient.delete('https://localhost:5001/api/project')
+  }
 }
