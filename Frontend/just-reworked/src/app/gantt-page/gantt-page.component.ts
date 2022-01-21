17ed1a21a9e03a8fe2b30cc006f026d5a7de38a8
samuel-kowatschek <samkow@gmx.at>
2022-01-21 20:59:07 +0100

    added custom borders, refined styling further

--- Frontend/just-reworked/src/app/gantt-page/gantt-page.component.ts
+++ Frontend/just-reworked/src/app/gantt-page/gantt-page.component.ts
@@ -67,10 +67,29 @@ export class GanttPageComponent implements OnInit {
 
   fillTableBackground(taskIdx: number, dayIdx: number) : string{
 
-    if(this.tableData[taskIdx].offset <= dayIdx && this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx){
+    if(this.tableData[taskIdx].offset <= dayIdx 
+        && this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx){
       return "#6b97ff"
     }
     return "white"
+  }
+
+  fillTableBorder(taskIdx: number, dayIdx: number) : string{
+    
+    
+
+    if(this.tableData[taskIdx].offset <= dayIdx 
+      && this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx){
+        if(this.tableData[taskIdx].offset == dayIdx){
+          return "20px 0 0 20px"
+        }else if(this.tableData[taskIdx].timespan+this.tableData[taskIdx].offset-1 == dayIdx){
+          return "0 20px 20px 0"
+        }
+        
+        
+      }
+    return "0px";
+
   }
 
   calcDuration(t: GetTask) : number {
