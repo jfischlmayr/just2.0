2ddada6e0e6c6e425bfc7dc9688aed2a777c2c2e
samuel-kowatschek <samkow@gmx.at>
2022-01-26 11:37:58 +0100

    fixed issue

--- Frontend/just-reworked/src/app/gantt-page/gantt-page.component.ts
+++ Frontend/just-reworked/src/app/gantt-page/gantt-page.component.ts
@@ -6,107 +6,144 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-gantt-page',
   templateUrl: './gantt-page.component.html',
-  styleUrls: ['./gantt-page.component.scss']
+  styleUrls: ['./gantt-page.component.scss'],
 })
 export class GanttPageComponent implements OnInit {
-  showDelay = new FormControl(500)
-  projects: GetProject[] = []
-  selectedProject? : GetProject
-  timespan = require('timespan')
-  tableData: TableData[] = []
-  tasks: GetTask[] = []
+  showDelay = new FormControl(500);
+  projects: GetProject[] = [];
+  selectedProject?: GetProject;
+  timespan = require('timespan');
+  tableData: TableData[] = [];
+  tasks: GetTask[] = [];
   days: number[] = [];
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
-    this.httpClient.get<GetProject[]>('https://localhost:5001/api/project').subscribe(result =>{
-      this.projects = result
-    });
-    this.httpClient.get<GetTask[]>('https://localhost:5001/api/task').subscribe(result =>{
-      this.tasks = result
-    });
-
+    this.httpClient
+      .get<GetProject[]>('https://localhost:5001/api/project')
+      .subscribe((result) => {
+        this.projects = result;
+      });
+    this.httpClient
+      .get<GetTask[]>('https://localhost:5001/api/task')
+      .subscribe((result) => {
+        this.tasks = result;
+      });
   }
 
-  downloadGantt(project: GetProject | undefined): void{
-    if(project)
-      this.httpClient.get(`https://localhost:5001/api/gantt/export?id=${project?.id}`).subscribe()
+  downloadGantt(project: GetProject | undefined): void {
+    if (project)
+      this.httpClient
+        .get(`https://localhost:5001/api/gantt/export?id=${project?.id}`)
+        .subscribe();
   }
 
-  calcDays(p : GetProject|undefined) : void{
-    var timeSpan = new this.timespan.TimeSpan()
-    this.days = []
-    this.tableData = []
+  calcDays(p: GetProject | undefined): void {
+    var timeSpan = new this.timespan.TimeSpan();
+    this.days = [];
+    this.tableData = [];
 
-    if(p){
-      var projTasks = this.tasks.filter(t => t.projectId == p?.id);
-      if(projTasks){
-        let dates = projTasks.map( p => new Date(p.startDate)).concat(projTasks.map( p => new Date(p.endDate)))
-        let start = dates.reduce(function (a, b) { return a < b ? a : b; });
-        let end = dates.reduce(function (a, b) { return a > b ? a : b; });
+    if (p) {
+      var projTasks = this.tasks.filter((t) => t.projectId == p?.id);
+      if (projTasks) {
+        let dates = projTasks
+          .map((p) => new Date(p.startDate))
+          .concat(projTasks.map((p) => new Date(p.endDate)));
+        let start = dates.reduce(function (a, b) {
+          return a < b ? a : b;
+        });
+        let end = dates.reduce(function (a, b) {
+          return a > b ? a : b;
+        });
 
-        timeSpan = this.timespan.fromDates(start, end, true)
+        timeSpan = this.timespan.fromDates(start, end, true);
 
         for (let i = 0; i < timeSpan.totalDays(); i++) {
-          this.days.push(i + 1)
+          this.days.push(i + 1);
         }
 
-        projTasks.forEach(t => {
-          let ts = this.timespan.fromDates(new Date(t.startDate), new Date(t.endDate), true)
-          let off = this.timespan.fromDates(new Date(t.startDate), start, true)
-          this.tableData.push({timespan: ts.totalDays(), offset: off.totalDays()})
-        })
+        projTasks.forEach((t) => {
+          let ts = this.timespan.fromDates(
+            new Date(t.startDate),
+            new Date(t.endDate),
+            true
+          );
+          let off = this.timespan.fromDates(new Date(t.startDate), start, true);
+          this.tableData.push({
+            timespan: ts.totalDays(),
+            offset: off.totalDays(),
+          });
+        });
       }
     }
   }
 
-  fillTableBackground(taskIdx: number, dayIdx: number) : string{
-
-    if(this.tableData[taskIdx].offset <= dayIdx
-        && this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx){
-      return "#6b97ff"
+  fillTableBackground(taskIdx: number, dayIdx: number): string {
+    if (
+      this.tableData[taskIdx].offset <= dayIdx &&
+      this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx
+    ) {
+      return '#6b97ff';
     }
-    return "white"
+    return 'white';
   }
 
-  fillTableBorder(taskIdx: number, dayIdx: number) : string{
-    if(this.tableData[taskIdx].offset <= dayIdx
-      && this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx){
-        if(this.tableData[taskIdx].offset == dayIdx){
-          return "20px 0 0 20px"
-        }else if(this.tableData[taskIdx].timespan+this.tableData[taskIdx].offset - 1 == dayIdx){
-          return "0 20px 20px 0"
-        }else if(this.tableData[taskIdx].timespan+ Math.round(this.tableData[taskIdx].offset) - 1   == dayIdx){
-          return "20px 20px 20px 20px"
-        }else if(this.tasksToShow()[taskIdx].title == "MOIN"){
-          console.log("timespan: "+this.tableData[taskIdx].timespan)
-          console.log("offset: "+Math.round(this.tableData[taskIdx].offset))
-          console.log("Day idx: " + dayIdx)
-          return "0px 0 20px 20px"
-        }
+  fillTableBorder(taskIdx: number, dayIdx: number): string {
+    if (
+      this.tableData[taskIdx].offset <= dayIdx &&
+      this.tableData[taskIdx].offset + this.tableData[taskIdx].timespan > dayIdx
+    ) {
+
+     /*console.log('timespan: ' + this.tableData[taskIdx].timespan);
+      console.log('offset: ' + this.tableData[taskIdx].offset);
+      console.log('day idx: ' + taskIdx);*/
+
+      if (this.tableData[taskIdx].offset == dayIdx) {
+        return '20px 0 0 20px';
+      } else if (
+        this.tableData[taskIdx].timespan + this.tableData[taskIdx].offset - 1 ==
+        dayIdx
+      ) {
+        return '0 20px 20px 0';
+      } else if (
+        this.tableData[taskIdx].timespan +
+          this.roundOffset(this.tableData[taskIdx].offset) -
+          1 ==
+        dayIdx
+      ) {
+        return '20px 20px 20px 20px';
       }
-    return "0px";
-
+    }
+    return '0px';
   }
 
-  calcDuration(t: GetTask) : number {
-    var timeSpan = new this.timespan.TimeSpan()
-    const start = new Date(t.startDate)
-    const end  = new Date(t.endDate)
-    timeSpan = this.timespan.fromDates(start, end, true)
-    return timeSpan.days
+  roundOffset(offset: number): number{
+    if(Math.round(offset) - offset > 0){
+      return Math.round(offset);
+    }
+    return Math.round(offset)+1;
+  }
+
+  calcDuration(t: GetTask): number {
+    var timeSpan = new this.timespan.TimeSpan();
+    const start = new Date(t.startDate);
+    const end = new Date(t.endDate);
+    timeSpan = this.timespan.fromDates(start, end, true);
+    return timeSpan.days;
   }
 
   tasksToShow(): GetTask[] {
     let tasks = [];
-    tasks = this.tasks.filter(t => t.projectId == this.selectedProject?.id);
+    tasks = this.tasks.filter((t) => t.projectId == this.selectedProject?.id);
     return tasks;
   }
 
-  showTooltipMessage(t: GetTask): String{
-    const start = new Date(t.startDate)
-    const end  = new Date(t.endDate)
-    return `${t.title}: ${start.toLocaleDateString()} - ${end.toLocaleDateString()}`
+  showTooltipMessage(t: GetTask): String {
+    const start = new Date(t.startDate);
+    const end = new Date(t.endDate);
+    return `${
+      t.title
+    }: ${start.toLocaleDateString()} - ${end.toLocaleDateString()}`;
   }
 }
