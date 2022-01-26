15dd09c8f0345b013c7051a50ac8bfc0be4596ca
jfischlmayr <jan.fischlmayr@gmx.net>
2022-01-26 08:20:24 +0100

    Code refactoring

--- Frontend/just-reworked/src/app/project-page/project-page.component.ts
+++ Frontend/just-reworked/src/app/project-page/project-page.component.ts
@@ -4,73 +4,89 @@ import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
-import { GetProject, Project } from '../model'
+import { GetProject, Project } from '../model';
 
 @Component({
   selector: 'app-project-page',
   templateUrl: './project-page.component.html',
-  styleUrls: ['./project-page.component.scss']
+  styleUrls: ['./project-page.component.scss'],
 })
 export class ProjectPageComponent implements OnInit {
   showDelay = new FormControl(500);
 
-  title : string = ''
-  startDate!: Date
-  endDate!: Date
-  description: string =''
+  title: string = '';
+  startDate!: Date;
+  endDate!: Date;
+  description: string = '';
 
-  editing: boolean = false
-  projectToEdit?: GetProject
+  editing: boolean = false;
+  projectToEdit?: GetProject;
 
-  projects?: GetProject[]
+  projects?: GetProject[];
 
-  constructor(private httpClient : HttpClient, private router: Router, public dialog: MatDialog) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    this.refresh()
+    this.refresh();
   }
 
   refresh() {
-    this.httpClient.get<GetProject[]>('https://localhost:5001/api/project').subscribe(result => {
-      this.projects = result
-    })
+    this.httpClient
+      .get<GetProject[]>('https://localhost:5001/api/project')
+      .subscribe((result) => {
+        this.projects = result;
+      });
   }
 
-  onSubmit(){
+  onSubmit() {
     const project: Project = {
       title: this.title,
       startDate: this.startDate,
       endDate: this.endDate,
-      description: this.description
-    }
-    console.log(project)
-    this.httpClient.post('https://localhost:5001/api/project', project).subscribe(() => this.refresh())
+      description: this.description,
+    };
+    console.log(project);
+    this.httpClient
+      .post('https://localhost:5001/api/project', project)
+      .subscribe(() => this.refresh());
 
-    this.title = ''
-    this.startDate = new Date()
-    this.endDate = new Date()
-    this.description = ''
+    this.title = '';
+    this.startDate = new Date();
+    this.endDate = new Date();
+    this.description = '';
   }
 
-  deleteProject(id: number){
-    this.httpClient.delete(`https://localhost:5001/api/project?id=${id}`).subscribe(() => this.refresh())
+  deleteProject(id: number) {
+    this.httpClient
+      .delete(`https://localhost:5001/api/project?id=${id}`)
+      .subscribe(() => this.refresh());
   }
 
-  editProject(p : GetProject) : void{
-    const dialogRef = this.dialog.open(EditDialogComponent,{
-      data:p,
-      panelClass: 'custom-dialog-container'
-    })
-    .afterClosed().subscribe( result => {
-      this.projectToEdit = result
+  editProject(p: GetProject): void {
+    const dialogRef = this.dialog
+      .open(EditDialogComponent, {
+        data: p,
+        panelClass: 'custom-dialog-container',
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        this.projectToEdit = result;
 
-      this.httpClient.put('https://localhost:5001/api/project', this.projectToEdit).subscribe(() => this.refresh())
-    })
+        this.httpClient
+          .put('https://localhost:5001/api/project', this.projectToEdit)
+          .subscribe(() => this.refresh());
+      });
   }
 
-  displayDate(p : GetProject) : string{
-    const sDate = p.startDate
-    const eDate = p.endDate
-    return `${p.startDate.toString().substring(0, 10)} bis ${p.endDate.toString().substring(0, 10)}`
+  displayDate(p: GetProject): string {
+    const sDate = p.startDate;
+    const eDate = p.endDate;
+    return `${p.startDate.toString().substring(0, 10)} bis ${p.endDate
+      .toString()
+      .substring(0, 10)}`;
   }
 }
--- Frontend/just-reworked/src/app/task-page/edit-task-dialog/edit-task-dialog.component.ts
+++ Frontend/just-reworked/src/app/task-page/edit-task-dialog/edit-task-dialog.component.ts
@@ -1,38 +1,48 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDatepickerContent } from '@angular/material/datepicker';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { GetTask, GetProject, GetEditTaskDialogData, Project, EditTask } from '../../model'
+import {
+  GetProject,
+  GetEditTaskDialogData,
+  EditTask,
+} from '../../model';
 
 @Component({
   selector: 'app-edit-task-dialog',
   templateUrl: './edit-task-dialog.component.html',
-  styleUrls: ['./edit-task-dialog.component.scss']
+  styleUrls: ['./edit-task-dialog.component.scss'],
 })
 export class EditTaskDialogComponent implements OnInit {
+  title: string = '';
+  startDate!: Date;
+  endDate!: Date;
+  selectedProjectId: number = 0;
+  projects: GetProject[] = [];
 
-  title : string = ''
-  startDate!: Date
-  endDate!: Date
-  selectedProjectId: number = 0
-  projects: GetProject[] = []
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: GetEditTaskDialogData, private dialogRef : MatDialogRef<EditTaskDialogComponent>) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: GetEditTaskDialogData,
+    private dialogRef: MatDialogRef<EditTaskDialogComponent>
+  ) {}
 
   ngOnInit(): void {
-    this.title = this.data.task.title
-    this.startDate = new Date(this.data.task.startDate)
-    this.endDate = new Date(this.data.task.endDate)
-    this.selectedProjectId = this.data.task.projectId
-    this.projects = this.data.projects
+    this.title = this.data.task.title;
+    this.startDate = new Date(this.data.task.startDate);
+    this.endDate = new Date(this.data.task.endDate);
+    this.selectedProjectId = this.data.task.projectId;
+    this.projects = this.data.projects;
   }
 
-  submitDialog() : void{
-    const editedTask: EditTask = {id: this.data.task.id, title: this.title, endDate: this.endDate, startDate: this.startDate, projectId: this.selectedProjectId}
-    this.dialogRef.close(editedTask)
+  submitDialog(): void {
+    const editedTask: EditTask = {
+      id: this.data.task.id,
+      title: this.title,
+      endDate: this.endDate,
+      startDate: this.startDate,
+      projectId: this.selectedProjectId,
+    };
+    this.dialogRef.close(editedTask);
   }
 
-  cancelDialog() : void{
-    this.dialogRef.close()
+  cancelDialog(): void {
+    this.dialogRef.close();
   }
-
 }
